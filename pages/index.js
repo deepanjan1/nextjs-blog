e450c2bf0d2ffc809aa34eccddc41e603d4a9ed8
Deepanjan De <deepanjan.de@gmail.com>
2022-12-30 21:54:11 -0500

    made small change on intro

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>My name is Deep.  I like to build things.</p>
+        <p>My name is Deep.  I like to build.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
